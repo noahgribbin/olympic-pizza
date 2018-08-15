3e1e8334176d5ea705124ca885ed9c19337fdc8a
Noah Gribbin <noahgribin@yahoo.com>
2018-08-15 16:28:23 -0700

    server app.listen

--- server.js
+++ server.js
@@ -6,3 +6,5 @@ app.use(express.static(`${__dirname}/dist`))
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'))
 })
+
+app.listen(process.env.PORT || 3000)
